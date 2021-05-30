e927cf8460a22f2e96804c1f182d4cf6cd0b12ed
stoq <63019703+sto9@users.noreply.github.com>
2021-05-31 02:47:03 +0900

    Update ForTampermonkey.js

--- ForTampermonkey.js
+++ ForTampermonkey.js
@@ -42,49 +42,46 @@ function main() {
     }
   }
 
-  window.onload = function () {
-    const url = location.href;
-    let gametype;
-    const regex = new RegExp('https://sdvx.in/sort/*');
-    if (regex.test(url)) gametype = 0;
-    else gametype = 1;
-    if(document.getElementById('extended') !== null) return;
-    const html_idx = [[1, 7], [4, 2]];
-    const start_index = [2, 2];
-    const html0 = document.getElementsByTagName('center')[0].getElementsByClassName('c')[html_idx[0][gametype]].getElementsByClassName('tbg')[0];
+  const url = location.href;
+  let gametype;
+  const regex = new RegExp('https://sdvx.in/sort/*');
+  if (regex.test(url)) gametype = 0;
+  else gametype = 1;
+  if (document.getElementById('extended') !== null) return;
+  const html_idx = [[1, 7], [4, 2]];
+  const start_index = [2, 2];
+  const html0 = document.getElementsByTagName('center')[0].getElementsByClassName('c')[html_idx[0][gametype]].getElementsByClassName('tbg')[0];
+  if (gametype == 0) {
+    html0.getElementsByTagName('tbody')[0].insertAdjacentHTML('afterbegin', '<tr><td></td><td></td><td></td><td style="text-align:right;"><div class="f3">※最新でない恐れあり</div></td></tr>');
+    html0.getElementsByTagName('tbody')[0].insertAdjacentHTML('afterbegin', '<tr><td></td><td></td><td></td><td style="text-align:right;"><div class="f1">S許容ニア数</div></td></tr>');
+  } else {
+    html0.getElementsByTagName('tbody')[0].insertAdjacentHTML(
+      'afterbegin',
+      '<tr><td></td><td><td style="text-align:right;"><div class="f2">SSS許容アタ数<br>(JUSTICE0の場合)</div></td>'
+      + '<td style = "text-align:right;"><div class="f2">99AJ<br>許容(J)</div></td>'
+    );
+  }
+  const html = html0.getElementsByTagName('tr');
+  for (let i = start_index[gametype]; i < html.length; i++) {
+    const td = html[i].getElementsByTagName('td');
+    if (td.length <= 1) continue;
+    const script_data = td[0].getElementsByTagName('script');
+    const src = script_data[0].getAttribute('src');
+    const ID = script_data[1].innerHTML.slice(4, 4 + 6);
+    const chain = parseInt(GetChain(ID, src));
+    if (isNaN(chain)) continue;
     if (gametype == 0) {
-      html0.getElementsByTagName('tbody')[0].insertAdjacentHTML('afterbegin', '<tr><td></td><td></td><td></td><td style="text-align:right;"><div class="f3">※最新でない恐れあり</div></td></tr>');
-      html0.getElementsByTagName('tbody')[0].insertAdjacentHTML('afterbegin', '<tr><td></td><td></td><td></td><td style="text-align:right;"><div class="f1">S許容ニア数</div></td></tr>');
+      td[html_idx[1][gametype]].insertAdjacentHTML('afterend', '<td style="text-align:right;"><div class="f1">' + CalcTolerance(chain, gametype) + '</div></td>');
     } else {
-      html0.getElementsByTagName('tbody')[0].insertAdjacentHTML(
-        'afterbegin',
-        '<tr><td></td><td><td style="text-align:right;"><div class="f2">SSS許容アタ数<br>(JUSTICE0の場合)</div></td>'
-        + '<td style = "text-align:right;"><div class="f2">99AJ<br>許容(J)</div></td>'
+      const result = CalcTolerance(chain, gametype);
+      td[html_idx[1][gametype]].insertAdjacentHTML(
+        'afterend',
+        '<td style="text-align:right;"><div class="f2">' + result[0] + '</div></td>'
+        + '<td style="text-align:right;"><div class="f2">' + result[1] + '</div></td>'
       );
     }
-    const html = html0.getElementsByTagName('tr');
-    for (let i = start_index[gametype]; i < html.length; i++) {
-      const td = html[i].getElementsByTagName('td');
-      if (td.length <= 1) continue;
-      const script_data = td[0].getElementsByTagName('script');
-      const src = script_data[0].getAttribute('src');
-      const ID = script_data[1].innerHTML.slice(4, 4 + 6);
-      const chain = parseInt(GetChain(ID, src));
-      if (isNaN(chain)) continue;
-      if (gametype == 0) {
-        td[html_idx[1][gametype]].insertAdjacentHTML('afterend', '<td style="text-align:right;"><div class="f1">' + CalcTolerance(chain, gametype) + '</div></td>');
-      } else {
-        const result = CalcTolerance(chain, gametype);
-        td[html_idx[1][gametype]].insertAdjacentHTML(
-          'afterend',
-          '<td style="text-align:right;"><div class="f2">' + result[0] + '</div></td>'
-          + '<td style="text-align:right;"><div class="f2">' + result[1] + '</div></td>'
-        );
-      }
-    }
-    document.getElementsByTagName('head')[0].setAttribute('id','extended');
   }
-
+  document.getElementsByTagName('head')[0].setAttribute('id', 'extended');
 }
 
 
