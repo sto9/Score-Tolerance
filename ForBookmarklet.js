e5cbf9e9aa8eb632a8343890c22cb58104c0b357
sto9 <63019703+sto9@users.noreply.github.com>
2023-12-08 03:37:11 +0900

    Update ForBookmarklet.js

--- ForBookmarklet.js
+++ ForBookmarklet.js
@@ -1,11 +1,14 @@
 (function () {
   'use strict';
-  var script = document.createElement('script');
-  script.setAttribute('src', 'https://code.jquery.com/jquery-1.12.4.min.js');
+  let jquery = document.createElement('script');
+  jquery.setAttribute('src', 'https://code.jquery.com/jquery-1.12.4.min.js');
+  document.head.appendChild(jquery);
+
+  let script = document.createElement('script');
   script.setAttribute('src', 'https://sto9.github.io/Score-Tolerance/function.js');
   script.setAttribute('type', 'text/javascript');
   script.addEventListener('load', function () {
-    $(function() {
+    $(function () {
       console.log('test')
     })
     UpdateTable();
