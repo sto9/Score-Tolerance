66f3c03098f83d3c2902d685fa7b4433c5675853
sto9 <63019703+sto9@users.noreply.github.com>
2023-12-08 01:46:03 +0900

    Update ForBookmarklet.js

--- ForBookmarklet.js
+++ ForBookmarklet.js
@@ -6,6 +6,7 @@ import { UpdateTable } from './function.js';
   var script = document.createElement('script');
   script.setAttribute('src', 'https://code.jquery.com/jquery-1.12.4.min.js');
   script.setAttribute('type', 'text/javascript');
+  script.setAttribute('type', 'module');
   script.addEventListener('load', function () {
     UpdateTable();
   });
