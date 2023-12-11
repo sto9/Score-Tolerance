8c40b6c8ffeeb198aa6fc6488ca59e74d04ce250
sto9 <63019703+sto9@users.noreply.github.com>
2023-12-11 09:53:29 +0900

    Update ForTampermonkey.js

--- ForTampermonkey.js
+++ ForTampermonkey.js
@@ -1,13 +1,11 @@
 // ==UserScript==
 // @name         Score-Tolerance
 // @namespace    http://tampermonkey.net/
-// @version      2.1
+// @version      2.2
 // @description  SDVX, CHUNITHM, オンゲキの譜面保管所で許容を表示します。
 // @description  （譜面保管所のデータを使用しているので、SDVXのデータは最新でない可能性があります）
 // @author       null
-// @include      /https:\/\/sdvx.in\/sort\/sort_[0-9].*/
-// @include      /https:\/\/sdvx.in\/chunithm\/sort\/[0-9].*/
-// @include      /https:\/\/sdvx.in\/ongeki\/sort\/[0-9].*/
+// @include      *sdvx.in*
 // @icon         
 // @grant        none
 // @require http://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js
